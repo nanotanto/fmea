4a12119616141162219519c4d807756a89799faf
Witanto <nano_tanto@yahoo.co.id>
2021-03-15 08:18:28 +0700

    Update otomatis AP
    Update code at add_conditions.js with automatically Action Priority (AP)

--- frontend/sources/views/p-fmea/add_condition.js
+++ frontend/sources/views/p-fmea/add_condition.js
@@ -22,7 +22,12 @@ export default class AddConditionView extends JetView{
                     { id:"form_condition","autoheight": false, "view": "form", "elementsConfig": { "required": false, "labelPosition": "top" },
 	"rows": [
 		{ id:"id_mode","view": "text", "label": "Mode ID", "name": "mode_id", hidden:true },
-		{ id:"mode_name","view": "text", "label": "Failure Mode (FM)", "name": "mode",readonly:true },
+        {
+            cols:[
+                { id:"mode_name","view": "text", "label": "Failure Mode (FM)", "name": "mode",readonly:true, gravity:3 },
+                { id:"severity","view": "text", "label": "Severity", "name": "mode",readonly:true, format:"111" },
+            ]
+        },		
 		{
 			"cols": [
 				{ "label": "Work Element", "options": ["","Man","Machine","Material",'Method','Measurement','Environment'], "view": "select", "name": "element" },
@@ -33,20 +38,114 @@ export default class AddConditionView extends JetView{
 		{ "label": "Prevention Control (PC) of FC", "view": "text", "name": "prevention" },
 		{
 			"cols": [
-				{ id:"o","label": "Occurrence (O)", "options":['1','2','3','4','5','6','7','8','9','10'], "view": "select", "name": "o" },
+				{ id:"o","label": "Occurrence (O)", "options":['1','2','3','4','5','6','7','8','9','10'], "view": "select", "name": "o", format:"111",
+                    on:{
+                        onItemClick: function(){     
+                            var S = $$("severity").getValue(); 
+                            var O = $$("o").getValue();     
+                            var D = $$("d").getValue(); 
+
+                            if (S >= 8 && O >= 8 && D >= 8) {
+                                $$("ap").define("options", [ 'High' ]);
+                                $$("ap").refresh();
+                            } else {
+                                $$("ap").define("options", [ 'Medium' ]);
+                                $$("ap").refresh();
+                            }                            
+                        }
+                      }
+                },
 				{ "view": "label", "height": 0, "gravity": 3 }
 			]
 		},
 		{ "label": "Detection Control (DC) of FC or FM", "view": "text", "name": "detection" },
 		{
 			"cols": [
-				{ id:"d","label": "Detection (D)", "options": ['1','2','3','4','5','6','7','8','9','10'], "view": "select", "name": "d" },
+				{ id:"d","label": "Detection (D)", "options": ['1','2','3','4','5','6','7','8','9','10'], "view": "select", "name": "d", format:"111",
+                    on:{
+                        onItemClick: function(){     
+                            var S = $$("severity").getValue(); 
+                            var O = $$("o").getValue();     
+                            var D = $$("d").getValue(); 
+
+                            if (S >= 9 && O >= 6 && D >= 1) {
+                                $$("ap").define("options", [ 'High' ]);
+                                $$("ap").refresh();
+
+                            } else if (S >= 9 && O >= 4 && D >= 2) {
+                                $$("ap").define("options", [ 'High' ]);
+                                $$("ap").refresh();
+
+                            } else if (S >= 9 && O >= 2 && D >= 7) {
+                                $$("ap").define("options", [ 'High' ]);
+                                $$("ap").refresh();
+
+                            } else if (S >= 7 && O >= 8 && D >= 1) {
+                                $$("ap").define("options", [ 'High' ]);
+                                $$("ap").refresh();
+
+                            } else if (S >= 7 && O >= 6 && D >= 2) {
+                                $$("ap").define("options", [ 'High' ]);
+                                $$("ap").refresh();
+
+                            } else if (S >= 7 && O >= 4 && D >= 7) {
+                                $$("ap").define("options", [ 'High' ]);
+                                $$("ap").refresh();
+
+                            } else if (S >= 4 && O >= 8 && D >= 5) {
+                                $$("ap").define("options", [ 'High' ]);
+                                $$("ap").refresh();
+
+                            } else if (S >= 9 && O >= 4 && D >= 1) {
+                                $$("ap").define("options", [ 'Medium' ]);
+                                $$("ap").refresh();
+
+                            } else if (S >= 9 && O >= 2 && D >= 5) {
+                                $$("ap").define("options", [ 'Medium' ]);
+                                $$("ap").refresh();
+
+                            } else if (S >= 7 && O >= 6 && D >= 1) {
+                                $$("ap").define("options", [ 'Medium' ]);
+                                $$("ap").refresh();
+
+                            } else if (S >= 7 && O >= 4 && D >= 1) {
+                                $$("ap").define("options", [ 'Medium' ]);
+                                $$("ap").refresh();
+
+                            } else if (S >= 7 && O >= 2 && D >= 5) {
+                                $$("ap").define("options", [ 'Medium' ]);
+                                $$("ap").refresh();
+
+                            } else if (S >= 4 && O >= 8 && D >= 2) {
+                                $$("ap").define("options", [ 'Medium' ]);
+                                $$("ap").refresh();
+
+                            } else if (S >= 4 && O >= 6 && D >= 1) {
+                                $$("ap").define("options", [ 'Medium' ]);
+                                $$("ap").refresh();
+
+                            } else if (S >= 4 && O >= 4 && D >= 7) {
+                                $$("ap").define("options", [ 'Medium' ]);
+                                $$("ap").refresh();
+
+                            } else if (S >= 2 && O >= 8 && D >= 5) {
+                                $$("ap").define("options", [ 'Medium' ]);
+                                $$("ap").refresh();
+
+                            } else {
+                                $$("ap").define("options", [ 'Low' ]);
+                                $$("ap").refresh();
+
+                            }              
+                        }
+                      }
+                },
 				{ "view": "label", "height": 0, "gravity": 3 }
 			]
 		},
 		{
 			"cols": [
-				{ "label": "Action Priority (AP)", "options": ["","Low","Medium","High"], "view": "select", "name": "ap" },
+				{ id:"ap","label": "Action Priority (AP)", options:[], "view": "select", "name": "ap" },
 				{ "view": "label", "gravity": 3 }
 			]
 		},
@@ -106,7 +205,10 @@ export default class AddConditionView extends JetView{
         var Select_mode = $$("tbl_modes_all").getSelectedItem();
         var mode_id = Select_mode['id'];
         var mode_name = Select_mode['mode'];
+        var s = Select_mode['s'];
         $$("id_mode").setValue(mode_id);  
-        $$("mode_name").setValue(mode_name);  
+        $$("mode_name").setValue(mode_name); 
+        $$("severity").setValue(s);  
     }
+
 }
